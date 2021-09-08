ffbd58e8ddbe23a30436eefc3b6754f470bd4733
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-09-08 15:05:18 +1000

    Make rooms a single json file so easier to update

---
+++ tools/monitor/index.js
@@ -0,0 +1,35 @@
+import { fetchRooms, viewerURI } from './fetchRooms.js';
+
+export const createRoomLink = (within) => (destination, label) => {
+  const base = document.createElement('li');
+  const link = document.createElement('a');
+
+  link.setAttribute('href', destination);
+
+  const text = document.createTextNode(label);
+  link.appendChild(text);
+
+  base.appendChild(link);
+
+  within.append(base);
+};
+
+
+const run = async () => {
+  console.log('hi');
+
+  const roomList = document.getElementById('room-list');
+  const roomLink = createRoomLink(roomList);
+
+  const { ok, rooms } = await fetchRooms();
+
+  if (!ok) {
+    alert('Could not get rooms. Try again.');
+  }
+
+  for (const { id, name } of rooms) {
+    roomLink(viewerURI(id), name);
+  }
+};
+
+run().catch(err => console.error('Failed somewhere', err));
